69a52f51fe72bc0ec8923c1c6ac6e4813f905aeb
Jerin Joy <jerinjoy004@gmail.com>
2025-03-12 07:11:01 +0530

    tried to setup notification, not tested

--- android/app/src/main/kotlin/com/example/tailmate/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tailmate/MainActivity.kt
@@ -3,3 +3,15 @@ package com.example.tailmate
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+
+private fun createNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channel = NotificationChannel(
+            "channel_id",
+            "Channel Name",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
+    }
+}
