eb2315a69426725855c2087486cb2133d17fced5
Jerin Joy <jerinjoy004@gmail.com>
2025-03-23 20:44:41 +0530

    modify

--- android/app/src/main/kotlin/com/example/tailmate/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tailmate/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.tailmate
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.content.Context
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-private fun createNotificationChannel() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        val channel = NotificationChannel(
-            "channel_id",
-            "Channel Name",
-            NotificationManager.IMPORTANCE_HIGH
-        )
-        val manager = getSystemService(NotificationManager::class.java)
-        manager.createNotificationChannel(channel)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "default_channel_id",
+                "Default Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
     }
 }
